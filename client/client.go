d01e82ae3a57f5a6463a2aadc9df51da1c87d39b
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-02-21 09:43:01 -0300

    client refactoring

--- client/client.go
+++ client/client.go
@@ -10,6 +10,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var Client pb.AddServiceClient
+
 func main() {
 	// Set up a connection to the server.
 	conn, err := Connection()
@@ -17,55 +19,62 @@ func main() {
 		log.Printf("failed to dial server %s: %v", *serverAddr, err)
 	}
 	defer conn.Close()
-	client := pb.NewAddServiceClient(conn)
-	g := gin.Default()
-	g.GET("/add/:a/:b", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
+	Client = pb.NewAddServiceClient(conn)
+	// Set up a http server.
+	router := gin.Default()
 
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-			return
-		}
-
-		req := &pb.Request{A: int64(a), B: int64(b)}
-		if response, err := client.Add(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-
+	router.GET("/", func(ctx *gin.Context) {
+		fmt.Fprintln(ctx.Writer, "Up and running...")
 	})
-	//---------------------------------------------------------------------------
-	g.GET("/mult/:a/:b", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-			return
-		}
-		req := &pb.Request{A: int64(a), B: int64(b)}
+	router.GET("/add/:a/:b", Add)
+	router.GET("/multiply/:a/:b", Multiply)
 
-		if response, err := client.Multiply(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	})
-
-	if err := g.Run(":8080"); err != nil {
+	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
+	}
+}
+
+func Add(ctx *gin.Context) {
+	a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
+		return
+	}
+
+	b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
+		return
+	}
+
+	req := &pb.Request{A: int64(a), B: int64(b)}
+	if response, err := Client.Add(ctx, req); err == nil {
+		ctx.JSON(http.StatusOK, gin.H{
+			"result": fmt.Sprint(response.Result),
+		})
+	} else {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+}
+
+func Multiply(ctx *gin.Context) {
+	a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
+		return
+	}
+	b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
+		return
+	}
+	req := &pb.Request{A: int64(a), B: int64(b)}
+
+	if response, err := Client.Multiply(ctx, req); err == nil {
+		ctx.JSON(http.StatusOK, gin.H{
+			"result": fmt.Sprint(response.Result),
+		})
+	} else {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 }
